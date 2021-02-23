cf3f573ed6d7487eaa113237ac9222b63edac09d
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2021-02-23 09:49:07 -0300

    Migrado para nextJs

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { homedir } from "os";
+import Head from "next/head";
+import { ExperienceBar } from "../components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
