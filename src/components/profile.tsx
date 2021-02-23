e937617a408b4de44cda87b45cf7f2d68363551b
Tarcisio Delmondes <tarcisiodelmondes@gmail.com>
2021-02-23 11:23:05 -0300

    ExperienceBar, Profile

---
+++ src/components/profile.tsx
@@ -0,0 +1,20 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://github.com/tarcisiodelmondes.png"
+        alt="Tarcisio Delmondes"
+      />
+
+      <div>
+        <strong>Tarcisio Delmondes</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
